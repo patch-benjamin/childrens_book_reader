475949acf79f4f9281c2265e0fc91775648f8894
Tyler Sun <tylersun10@gmail.com>
2023-07-20 08:57:22 -0600

    Initial Commit

---
+++ ReadingRainbow/ReadingRainbowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingRainbowApp.swift
+//  ReadingRainbow
+//
+//  Created by Tyler Sun on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadingRainbowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
