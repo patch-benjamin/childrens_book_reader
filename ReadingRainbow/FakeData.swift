307851512db001f960a6599ac8e540e5fc3d7345
ami-smith <111449179+ami-smith@users.noreply.github.com>
2023-09-07 21:38:03 -0600

    debugging fake data

--- ReadingRainbow/FakeData.swift
+++ ReadingRainbow/FakeData.swift
@@ -12,23 +12,32 @@ extension Book {
     struct Fake {
         static let books = [book1, book2, book3]
         
-        static let book1ImageName = "Harry Potter Cover"
-        static let book1Image = UIImage(named: book1ImageName)
+        static let book1Image = UIImage(named: "book1Image")?.pngData() ?? Data()
+        static let book2Image = UIImage(named: "book2Image")?.pngData() ?? Data()
+        static let book3Image = UIImage(named: "book3Image")?.pngData() ?? Data()
         
-        static let book2ImageName = "Would You Rather Cover"
-        static let book2Image = UIImage(named: book2ImageName)
+        static let book1Content = [
+            Page(image: Data(), pageNumber: 1, content: ["Page 1 Text"]),
+            Page(image: Data(), pageNumber: 2, content: ["Page 2 Text"])
+        ]
         
-        static let book3ImageName = "The Hobbit Cover"
-        static let book3Image = UIImage(named: book3ImageName)
+        static let book2Content = [
+            Page(image: Data(), pageNumber: 1, content: ["Page 1 Text"]),
+            Page(image: Data(), pageNumber: 2, content: ["Page 2 Text"])
+        ]
+        
+        static let book3Content = [
+            Page(image: Data(), pageNumber: 1, content: ["Page 1 Text"]),
+            Page(image: Data(), pageNumber: 2, content: ["Page 2 Text"])
+        ]
+        
+        static let book1 = Book(image: book1Image, title: "Harry Potter and the Sorcerer's Stone", pages: book1Content, uniqueWords: ["magic", "wizard", "spell"], paragraphs: ["Paragraph 1 Text", "Paragraph 2 Text"])
         
         
-        static let book1 = Book(image: book1Image?.pngData() ?? Data(), title: "Harry Potter and the Sorcerer's Stone", pages: [Page(pageNumber: 1), Page(pageNumber: 2)], uniqueWords: ["magic", "wizard", "spell"], paragraphs: ["Paragraph 1 Text", "Paragraph 2 Text"])
+        static let book2 = Book(image: book2Image, title: "Would You Rather...?", pages: book2Content, uniqueWords: ["question", "choice", "fun"], paragraphs: ["Paragraph 1 Text", "Paragraph 2 Text"])
         
         
-        static let book2 = Book(image: book2Image?.pngData() ?? Data(), title: "Would You Rather...?", pages: [Page(pageNumber: 1), Page(pageNumber: 2)], uniqueWords: ["question", "choice", "fun"], paragraphs: ["Paragraph 1 Text", "Paragraph 2 Text"])
-        
-        
-        static let book3 = Book(image: book3Image?.pngData() ?? Data(), title: "The Hobbit", pages: [Page(pageNumber: 1), Page(pageNumber: 2)], uniqueWords: ["hobbit", "adventure", "dragon"], paragraphs: ["Paragraph 1 Text", "Paragraph 2 Text"])
+        static let book3 = Book(image: book3Image, title: "The Hobbit", pages: book3Content, uniqueWords: ["hobbit", "adventure", "dragon"], paragraphs: ["Paragraph 1 Text", "Paragraph 2 Text"])
     }
 }
 
--- ReadingRainbow/Models/Page.swift
+++ ReadingRainbow/Models/Page.swift
@@ -8,8 +8,9 @@
 import Foundation
 
 struct Page {
-    var image: Data
-    var pageNumber: Int // maybe not needed?
-    var uniqueWords: [String] // computed from paragraphs?
-    var paragraphs: [String]
+    var image: Data?
+    var pageNumber: Int? // maybe not needed?
+    var uniqueWords: [String]? // computed from paragraphs?
+    var paragraphs: [String]?
+    var content: [String]?
 }
