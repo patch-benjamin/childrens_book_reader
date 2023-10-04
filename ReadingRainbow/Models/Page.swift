f649661c272a1109dd51d9692e63a57c2bfb65e0
ami-smith <111449179+ami-smith@users.noreply.github.com>
2023-10-03 18:52:44 -0600

    adding different folders for assets
    got rid of optionals in the pages struct

--- ReadingRainbow/Models/Page.swift
+++ ReadingRainbow/Models/Page.swift
@@ -8,9 +8,8 @@
 import Foundation
 
 struct Page {
-    var image: Data?
-    var pageNumber: Int? // maybe not needed?
-    var uniqueWords: [String]? // computed from paragraphs?
-    var paragraphs: [String]?
-    var content: [String]?
+    var image: Data
+    var pageNumber: Int // maybe not needed?
+    var uniqueWords: [String] // computed from paragraphs? grab all unique words from its pages and return those. a set would be better, remove punctuation and anything that's not alphabet or number, trim away characters that aren't alphanumeric at the beginning or end
+    var paragraphs: [String]
 }
